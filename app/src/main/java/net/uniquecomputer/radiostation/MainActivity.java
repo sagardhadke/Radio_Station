acb6b1748206652563368621f31fe2800fe1ff8d
Sagar Dhadke <peaceunique2003@gmail.com>
2022-08-16 17:20:49 +0530

    Added Menu

--- app/src/main/java/net/uniquecomputer/radiostation/MainActivity.java
+++ app/src/main/java/net/uniquecomputer/radiostation/MainActivity.java
@@ -1,8 +1,12 @@
 package net.uniquecomputer.radiostation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize And Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        //Perform ItemSelectedLister
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+
+                }
+                return false;
+            }
+        });
     }
 }
