7363c625fb6e5dbfa99e53996e2d18c95d481378
Sagar Dhadke <peaceunique2003@gmail.com>
2022-08-16 19:58:45 +0530

    Added Profile.java

---
+++ app/src/main/java/net/uniquecomputer/radiostation/Song.java
@@ -0,0 +1,32 @@
+package net.uniquecomputer.radiostation;
+
+public class Song {
+
+    private String title;
+
+
+    // why are you using int for image "your images in drawable folder"
+    private int image;
+
+    public Song(String title, int image) {
+        this.title = title;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+}
---
+++ app/src/main/java/net/uniquecomputer/radiostation/SongAdapter.java
@@ -0,0 +1,4 @@
+package net.uniquecomputer.radiostation;
+
+public class SongAdapter {
+}
